7b47d3924c54a672ee6bdc1017df7716bdb036dd
jd <jewdev@cock.email>
2021-07-16 14:59:05 +0300

    added threads to make it faster lol

--- scam/scam.py
+++ scam/scam.py
@@ -3,26 +3,42 @@ import os
 import random
 import string
 import json
+import threading
 
 chars = string.ascii_letters + string.digits + 'abcdefghijklmnopqrstuvwxyz!@#$%^&*()'
 random.seed = (os.urandom(1024))
 
 url = input('Enter URL: ')
-postUsername = input('Enter username form data: ')
+postEmail = input('Enter email form data: ')
 postPassword = input('Enter password form data: ')
+threadsAmount = int(input('Enter amount of threads: '))
 
 os.system('cls')
 
 names = json.loads(open('names.json').read())
 
-for name in names:
-    name_extra = ''.join(random.choice(string.digits))
+def request():
+    for name in names:
+        name_extra = ''.join(random.choice(string.digits))
 
-    username = name.lower() + name_extra + '@gmail.com'
-    password = ''.join(random.choice(chars) for i in range(8))
+        email = name.lower() + name_extra + '@gmail.com'
+        password = ''.join(random.choice(chars) for i in range(8))
 
-    requests.post(url, allow_redirects = False, data = {
-        postUsername: username,
-        postPassword: password
-    })
-    print('sending username %s and password %s' % (username, password))
+        requests.post(url, allow_redirects = False, data = {
+            postEmail: email,
+            postPassword: password
+        })
+        print('sending email %s and password %s' % (email, password))
+
+threads = []
+
+for i in range(threadsAmount):
+    t = threading.Thread(target=request)
+    t.daemon = True
+    threads.append(t)
+
+for i in range(threadsAmount):
+    threads[i].start()
+
+for i in range(threadsAmount):
+    threads[i].join()
